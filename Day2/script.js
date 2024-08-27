aa8fd2a2e864963859594a7923e8612883a61535
johnsamrat200 <johnsamrat200@gmail.com>
2024-08-27 16:32:40 +0530

    practice js and task

--- Day2/script.js
+++ Day2/script.js
@@ -1,8 +1,51 @@
-function jo() {
-    console.log("hello");
-    function john(){
-        console.log("john");
-    }
+//Alert 
+alert("Welcome to john's website");
+//Prompts
+let names = prompt("What is your name?");
+alert("Hello ," + names + "!");
+//Arithemetic operators
+let a=100;
+let b=6;
+ // addition
+console.log(a+b);
+// subraction
+console.log(a-b);
+//multiplication
+console.log(a*b);
+//division
+console.log(a/b);
+//modulus
+console.log(a%b);
+//exponentiation
+console.log(a**b);
+//Logical Operators
+let a = true;
+let b = false;
+//AND operator
+console.log(a&&b);
+//OR Operator
+console.log(a||b);
+//NOT operator
+console.log(!a);
+//Variables
+var name = "john";
+let age = 26;
+const country = "Tamilnadu";
 
+console.log(name);  
+console.log(age); 
+console.log(country); 
+
+
+
+
+//Interview Question As Task.
+function outer(){
+    console.log("john");
+    function inner(){
+        console.log("samrat");
+
+    }return inner();
 }
-jo();
+let inner1 = outer();
+
