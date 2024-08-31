faf4b4e0b73acf64f08a1f084d844f8a46c9fa16
johnsamrat200 <johnsamrat200@gmail.com>
2024-08-31 22:05:13 +0530

    updated

--- john'sFSD/script.js
+++ john'sFSD/script.js
@@ -1,51 +1,79 @@
-//Alert 
-alert("Welcome to john's website");
-//Prompts
-let names = prompt("What is your name?");
-alert("Hello ," + names + "!");
-//Arithemetic operators
-let a=100;
-let b=6;
- // addition
-console.log(a+b);
-// subraction
-console.log(a-b);
-//multiplication
-console.log(a*b);
-//division
-console.log(a/b);
-//modulus
-console.log(a%b);
-//exponentiation
-console.log(a**b);
-//Logical Operators
-let a = true;
-let b = false;
-//AND operator
-console.log(a&&b);
-//OR Operator
-console.log(a||b);
-//NOT operator
-console.log(!a);
-//Variables
-var name = "john";
-let age = 26;
-const country = "Tamilnadu";
+// //Alert 
+// alert("Welcome to john's website");
+// //Prompts
+// let names = prompt("What is your name?");
+// alert("Hello ," + names + "!");
+// //Arithemetic operators
+// let a=100;
+// let b=6;
+//  // addition
+// console.log(a+b);
+// // subraction
+// console.log(a-b);
+// //multiplication
+// console.log(a*b);
+// //division
+// console.log(a/b);
+// //modulus
+// console.log(a%b);
+// //exponentiation
+// console.log(a**b);
+// //Logical Operators
+// let a = true;
+// let b = false;
+// //AND operator
+// console.log(a&&b);
+// //OR Operator
+// console.log(a||b);
+// //NOT operator
+// console.log(!a);
+// //Variables
+////var is Function Scoped/Globally Scoped.hoisted and initialized with undefined.
+// var name = "john";
+// console.log(name); 
+////let is block-scoped.hoisted but not initialized.
+// let age = 26;
+// console.log(age); 
+////const is block-scoped constant ,cannot be reassigned.
+// const country = "Tamilnadu";
+// console.log(country);
+////datatypes:-
+////string
+//let greeting="Hello,World!";
+////number:-
+//let score = 100;
+//let temperature = 50.4;
+////boolean:-
+//let isLoggedIn = true;
+//let isLoggedIn = false;
+////null:-
+//let empty = null;
+////Object:-A Complex Datatype Used To Store Collections of Data.
+/*let person = {
+name:"John",
+age:"26",
+isStudent:false
+};
+*/
+////Array:-A Special Type of Object Used to Store Lists of Values.
+// let numbers = [1,2,3,4,5,6,7,8,9];
 
-console.log(name);  
-console.log(age); 
-console.log(country); 
+
+
+ 
+
+ 
 
 
 
 
-//Interview Question As Task.
-function outer(){
-    console.log("john");
-    function inner(){
-        console.log("samrat");
+// //Interview Question As Task.
+// function outer(){
+//     console.log("john");
+//     function inner(){
+//         console.log("samrat");
 
-    }return inner();
-}
-let inner1 = outer();
+//     }return inner();
+// }
+// let inner1 = outer();
 
